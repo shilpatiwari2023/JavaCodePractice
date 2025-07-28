2afa5698b5775144b1b458adcd47202d265a4dce
Shilpa Tiwari <shilpatiwari08@gmail.com>
2025-07-28 12:34:02 +0530

    java code

--- src/main/java/string/RemoveLeadingZero.java
+++ src/main/java/string/RemoveLeadingZero.java
@@ -5,17 +5,23 @@ import java.sql.SQLOutput;
 public class RemoveLeadingZero {
     public static void main(String[] args) {
         String str = "0000012345";
+        //StringBuffer sb1 = new StringBuffer(str);
+        //System.out.println(sb1.replace(0,5,""));
         str = removeZeroFromStart(str);
         System.out.println("Result string without zero is : " + str);
     }
 
     private static String removeZeroFromStart(String str) {
         StringBuffer sb = new StringBuffer(str);
+        int endIndex = 0;
         for(int i = 0; i < str.length(); i++){
             if(str.charAt(i)=='0'){
-                sb.replace(0,i," ");
+                endIndex = i;
+            } else {
+                break;
             }
         }
+        sb.replace(0, endIndex+1,"");
         return sb.toString();
     }
 
--- src/main/java/string/SubstringOfAString.java
+++ src/main/java/string/SubstringOfAString.java
@@ -2,40 +2,17 @@ package src.main.java.string;
 
 import java.util.ArrayList;
 import java.io.*;
+/*
 
+ */
 public class SubstringOfAString {
     public static void main(String[] args) {
         String stringInput = new String("The Cat");
-        ArrayList<String> subStringList = SubstringOfAString.splitSubstrings(stringInput);
-        System.out.println("\nSubstring list printed as an ArrayList : ");
-        System.out.println(subStringList);
-        System.out.println("\n\nAll substrings printed 1 per line : ");
-        int count = 1;
-        // each substring would be printed
-        // within double quotes
-        for (String it : subStringList) {
-            System.out.println("(" + count + ") \"" + it + "\"");
-            count++;
+        String subString = null;
+        //System.out.println(stringInput.substring(0,0));
+        for(int i =1 ; i<=stringInput.length(); i++){
+            subString = stringInput.substring(0,i);
+            System.out.println(subString);
         }
-        // function to split a string into all its substrings
-        // and return the list as an object of ArrayList
-        public static ArrayList<String> splitSubstrings (String s){
-            // variables to traverse through the string
-            int i, j;
-            // to store the length of the string
-            int stringLength = s.length();
-            // List object to store the list of
-            // all substrings of the string s
-            ArrayList<String> subStringList = new ArrayList<String>();
-            // first for loop
-            for (i = 0; i < stringLength; i++) {
-                for (j = i + 1; j <= stringLength; j++) {
-                    subStringList.add(s.substring(i, j));
-                }
-            } // end of first for loop
-            // returning the list (object of ArrayList) of substrings of string s
-            return subStringList;
-        }
-
     }
 }
